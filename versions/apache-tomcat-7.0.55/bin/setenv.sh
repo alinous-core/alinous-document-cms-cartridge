1ae8464cbab87f4b875e7602a0e41fcd1e8bfdda
iizuka <iizuka@opem-ec.jp>
2014-08-13 17:11:22 +0900

    configure java vm

--- versions/apache-tomcat-7.0.55/bin/setenv.sh
+++ versions/apache-tomcat-7.0.55/bin/setenv.sh
@@ -3,4 +3,7 @@
 export ALINOUS_HOME=${OPENSHIFT_ALINOUS_DIR}ALINOUS_HOME/
 export CATALINA_PID=${OPENSHIFT_ALINOUS_DIR}run/alinous.pid
 
+export JAVA_OPTS="-server -XX:PermSize=32M -XX:MaxPermSize=32m -Xms256m -Xmx256m -XX:NewRatio=1 -XX:SurvivorRatio=32"
+
+
 
